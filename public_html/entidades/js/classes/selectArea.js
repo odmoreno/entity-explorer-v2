32353431f7429b565a21b6383cf8e5ec4ed007b3
oscar.moreno <oscar.moreno@cti.espol.edu.ec>
2021-10-12 14:10:29 -0500

    Cambios move

--- public_html/entidades/js/classes/selectArea.js
+++ public_html/entidades/js/classes/selectArea.js
@@ -33,10 +33,10 @@ class DatePicker {
   }
 
   update = (currentDate) => {
-      console.log("picker:", this.datepicker)
+      //console.log("picker:", this.datepicker)
       const value = currentDate.toLocaleDateString("es-ES")
-      console.log(currentDate)
-      console.log(value)
+      //console.log(currentDate)
+      //console.log(value)
       this.datepicker.value = value
       $(`#${this.id}`).datepicker('update', currentDate)
       //this.datepicker.setDate(currentDate)
@@ -45,11 +45,16 @@ class DatePicker {
 }
 
 class Brush extends Component{
+
+  isDown = false;
+  offset = [0, 0];
+  
   constructor(hostElementId){
     super(hostElementId);
     this.id = hostElementId
     this.hostElement = document.getElementById(hostElementId);
     //this.create();
+    //this.connectMouseEvent();
   }
 
   closeNotifierHandler = (closeNotifierFunction) => {
@@ -114,19 +119,48 @@ class Brush extends Component{
                 .style('width', Math.abs(newX - this.x) + 'px')
                 .style('height', Math.abs(newY - this.y) + 'px')
   }
+
+  connectMouseEvent = () => {
+    console.log(this);
+    console.log(this.element);
+
+    this.element.addEventListener("mousedown", function (e) {
+      this.isDown = true;
+      console.log("mouse down")
+      //this.offset = [
+        //  divOverlay.offsetLeft - e.clientX,
+        //  divOverlay.offsetTop - e.clientY,
+        //];
+      });
+    
+    this.element.addEventListener("mouseup", function () {
+        this.isDown = false;
+        console.log("mouse up")
+    });
+
+    this.element.addEventListener("mousemove", function (e) {
+      e.preventDefault();
+      if (this.isDown) {
+        console.log("mouse move")
+        //divOverlay.style.left = e.clientX + offset[0] + "px";
+        //divOverlay.style.top = e.clientY + offset[1] + "px";
+      }
+    });
+  }
   
 }
 
 class SelectionArea {
   hasActiveBrush = false;
   hasOneBrush = false;
+  
 
-  constructor(id){
+  constructor(id) {
     this.id = id;
-    this.datesLimit = {}
-    this.drawArea = document.getElementById('canvas');
-    this.startDate = new DatePicker('datepicker1')
-    this.endDate = new DatePicker('datepicker2')
+    this.datesLimit = {};
+    this.drawArea = document.getElementById("canvas");
+    this.startDate = new DatePicker("datepicker1");
+    this.endDate = new DatePicker("datepicker2");
     this.connectClickEvent();
     this.connectMouseEvent();
     this.connectDraggable();
@@ -136,62 +170,58 @@ class SelectionArea {
   startRect = (e) => {
     //console.log("Click in canvas", e)
     //console.log(this)
-    if(this.hasActiveBrush){
-      this.drawArea.style.cursor = "default"
-      this.hasActiveBrush = false
-      globalThis.hasOpenBrush = false
+    if (this.hasActiveBrush) {
+      this.drawArea.style.cursor = "default";
+      this.hasActiveBrush = false;
+      globalThis.hasOpenBrush = false;
       this.brush.attach(false);
-      console.log("Dates:", this.datesLimit)
-      this.getSessionsInRange()
+      this.brush.connectMouseEvent()
+      //console.log("Dates:", this.datesLimit)
+      this.getSessionsInRange();
       return;
     }
-    if(!this.hasOneBrush){
-
+    if (!this.hasOneBrush) {
       this.brush.closeNotifierHandler(() => {
         this.hasActiveBrush = false;
         this.hasOneBrush = false;
       });
-      this.brush.eventHandler(e)
+      this.brush.eventHandler(e);
       this.brush.create();
       this.brush.attach(true);
+      
       //this.brush.init( () => { this.hasActiveBrush = false;}, e )
-  
-      this.drawArea.style.cursor = 'crosshair';
-      this.hasActiveBrush = true;
-      this.hasOneBrush = true
-      globalThis.hasOpenBrush = true
 
+      this.drawArea.style.cursor = "crosshair";
+      this.hasActiveBrush = true;
+      this.hasOneBrush = true;
+      globalThis.hasOpenBrush = true;
     }
-    e.stopPropagation()
-    
-  }
+    e.stopPropagation();
+  };
 
   setMousePos = (e) => {
-    if(this.hasActiveBrush && this.hasOneBrush){
+    if (this.hasActiveBrush && this.hasOneBrush) {
       //console.log("mouse move in canvas")
       var properties = shortTimeline.getEventProperties(e);
       // properties contains things like node id, group, x, y, time, etc.
-      console.log('mousemove properties:', properties.time);
-      this.getDatesRange(properties)
-      this.brush.update(e)
+      //console.log('mousemove properties:', properties.time);
+      this.getDatesRange(properties);
+      this.brush.update(e);
     }
-  
-  }
+  };
 
   getDatesRange(properties) {
-    if(!this.datesLimit['first']){
-      this.datesLimit['first'] = properties.time
-      this.startDate.update(properties.time)
-    }
-    else{
-      this.datesLimit['last'] = properties.time
-      this.endDate.update(properties.time)
+    if (!this.datesLimit["first"]) {
+      this.datesLimit["first"] = properties.time;
+      this.startDate.update(properties.time);
+    } else {
+      this.datesLimit["last"] = properties.time;
+      this.endDate.update(properties.time);
     }
   }
 
   getSessionsInRange = () => {
-    
-    let firstDay, lastDay 
+    let firstDay, lastDay;
 
     //if(globalThis.datesTimeline['first']<globalThis.datesTimeline['last']){
     //  firstDay = globalThis.datesTimeline['first']
@@ -201,54 +231,52 @@ class SelectionArea {
     //  firstDay = globalThis.datesTimeline['last']
     //  lastDay = globalThis.datesTimeline['first']
     //}
-    firstDay = this.datesLimit['first']
-    lastDay = this.datesLimit['last']
-    
-    console.log(firstDay)
-    console.log(lastDay)
+    firstDay = this.datesLimit["first"];
+    lastDay = this.datesLimit["last"];
+
+    console.log(firstDay);
+    console.log(lastDay);
     //console.log(sesiones)
-    this.votaciones = []
+    this.votaciones = [];
 
-    Object.values(sesiones).forEach(v => {
+    Object.values(sesiones).forEach((v) => {
       //console.log(v)
-      let fecha = v.fecha.split("-"); 
-      let vDate = new Date(fecha[0], fecha[1]- 1, fecha[2]); 
-      if(vDate>= firstDay && vDate<= lastDay){
+      let fecha = v.fecha.split("-");
+      let vDate = new Date(fecha[0], fecha[1] - 1, fecha[2]);
+      if (vDate >= firstDay && vDate <= lastDay) {
         //console.log(vDate)
-        this.votaciones.push(v)
+        this.votaciones.push(v);
       }
-    })
-    console.log(this.votaciones)
-    listaResultados.innerHTML = ''
-    outputVotes(this.votaciones)
-    dictIds["yVotos"] = this.votaciones
-    delete this.datesLimit['first']
+    });
+    console.log(this.votaciones);
+    listaResultados.innerHTML = "";
+    outputVotes(this.votaciones);
+    dictIds["yVotos"] = this.votaciones;
+    delete this.datesLimit["first"];
 
-    console.log(this.datesLimit)
-  }
+    console.log(this.datesLimit);
+  };
 
-  connectDraggable(){
-    this.drawArea.addEventListener('dragstart', event => {
-      event.dataTransfer.setData('text/plain', 'yVotos');
-      event.dataTransfer.effectAllowed = 'move';
+  connectDraggable() {
+    this.drawArea.addEventListener("dragstart", (event) => {
+      event.dataTransfer.setData("text/plain", "yVotos");
+      event.dataTransfer.effectAllowed = "move";
+      console.log("drag start", event);
     });
 
-    this.drawArea.addEventListener('dragend', event => {
-      console.log("Drag END")
+    this.drawArea.addEventListener("dragend", (event) => {
+      console.log("Drag END", event);
       console.log(event);
-
     });
   }
 
-  connectClickEvent(){
-    this.drawArea.addEventListener('click', this.startRect)
+  connectClickEvent() {
+    this.drawArea.addEventListener("click", this.startRect);
   }
 
-  connectMouseEvent(){
-    this.drawArea.addEventListener('mousemove', this.setMousePos)
+  connectMouseEvent() {
+    this.drawArea.addEventListener("mousemove", this.setMousePos);
   }
-
-
 }
 
 const divArea = new SelectionArea('canvas')
